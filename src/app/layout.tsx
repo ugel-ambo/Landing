0c1d3ac2b0620ac9aa87c35f79ab20d05fc2bb9d
jhonatan-aj <an.jhonatan15@gmail.com>
2025-10-28 09:15:06 -0500

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Menu from "./(Landing)/menu";
 import Footer from "./(Landing)/footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         <Menu />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
