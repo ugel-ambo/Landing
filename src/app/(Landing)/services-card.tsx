8f158bf31a8f752b49f34e729d677e1a55cc43f2
ANONIMUS-B <ayalaromerojordanbrandon@gmail.com>
2026-06-30 13:27:16 -0500

    Directorio Enlace

--- src/app/(Landing)/services-card.tsx
+++ src/app/(Landing)/services-card.tsx
@@ -75,8 +75,8 @@ const services = [
     title: "Directorio",
     icon: BookUser,
     image: "/directorio.png",
-    href: "https://drive.google.com/file/d/1Ax06_qv2BbasgBsPAhfv6M4PfunODfl2/view?usp=sharing",
-    external: true,
+    href: "/Nosotros/Directorio",
+    external: false,
   },
 ];
 
